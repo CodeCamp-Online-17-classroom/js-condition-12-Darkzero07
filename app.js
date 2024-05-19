3063da0c57e67c030c9f1870d1ab68be83322684
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 10:49:11 +0700

    js lab 29

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+let zone = prompt("Enter zone : ");
+if (zone.length !== 2) {
+    prompt("Enter zone : ");
+}else  {
+    switch (zone) {
+        case 'A1': alert('200')
+        break;
+        case 'A2': alert('300')
+        break;
+        case 'A3': alert('400')
+        break;
+    }
+}
+
